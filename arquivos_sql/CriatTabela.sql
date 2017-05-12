7e7ab49852af42bd0e67dc98e3476e4706c75139
Tiago Oliveira <tiago@econtabil.com>
2017-05-11 21:32:11 -0300

    first commit

---
+++ arquivos_sql/CriatTabela.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  Arquivo criado - Quinta-feira-Maio-11-2017   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table TIAGO_ALUNO
+--------------------------------------------------------
+
+  CREATE TABLE "SYSTEM"."TIAGO_ALUNO" 
+   (	"CODIGO" NUMBER(*,0), 
+	"NOME" VARCHAR2(20 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
---
+++ src/com/opet/Main.java
@@ -0,0 +1,60 @@
+package com.opet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.opet.util.Reader;
+public class Main
+{
+
+
+    public static void main(String[] args) throws Exception
+    {
+        // TODO Auto-generated method stub
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe",
+                        "system","system");
+        int opcao = 0;
+        while(opcao != 9 ) {
+            System.out.println("Escolha uma opção");
+            System.out.println("1 - Inserir aluno");
+            System.out.println("2 - Listar Aluno");
+            System.out.println("9 - Sair");
+            opcao = Reader.readInt();
+
+            switch (opcao)
+            {
+                case 1:
+                    System.out.println("Digite o nome do aluno:");
+                    String nomeAluno = Reader.readString();
+
+                    PreparedStatement stmt =
+                                    conn.prepareStatement("insert into tiago_aluno (codigo, nome) values ("
+                                                    + "(select coalesce(max(codigo)+1,1) from tiago_aluno), ?)");
+                    //stmt.setInt(1,1);
+                    stmt.setString(1,nomeAluno);
+                    try {
+                        ResultSet rs = stmt.executeQuery();
+                        System.out.println("Registro gravado com sucesso.");
+
+                    } catch(Exception e) {
+                        System.out.println("Erro ao gravar registro.");
+                    }
+
+                    break;
+                case 2:
+                    System.out.println("Não implementado");
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+    }
+
+}
